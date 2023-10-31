320eea4f65b8dcda8e9d5640fc15e700021d480c
edriso <edriso.dev@gmail.com>
2023-10-31 19:44:03 +0200

    refactor solution

--- Programming Skills/1768. Merge Strings Alternately.js
+++ Programming Skills/1768. Merge Strings Alternately.js
@@ -7,21 +7,17 @@
 const mergeAlternately = function (word1, word2) {
   if (word1.length < 1 || word2.length > 100) return;
 
-  const lengthDifference = word1.length - word2.length;
-  const shorterLength = lengthDifference >= 0 ? word2.length : word1.length;
-
   let merged = '';
-  for (let i = 0; i < shorterLength; i++) {
+  const minLength = Math.min(word1.length, word2.length);
+
+  for (let i = 0; i < minLength; i++) {
     merged += word1[i] + word2[i];
   }
 
-  if (lengthDifference === 0) return merged;
+  merged += word1.slice(minLength) + word2.slice(minLength);
 
-  merged +=
-    lengthDifference >= 0
-      ? word1.slice(-lengthDifference)
-      : word2.slice(lengthDifference);
   return merged;
 };
 
-console.log(mergeAlternately('ab', 'pqrs'));
+console.log(mergeAlternately('ab', 'pqrs')); //apbqrs
+console.log(mergeAlternately('pqrsd', 'ab')); //paqbrsd
