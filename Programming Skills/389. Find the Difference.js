457a69bb8f208bbc61420a50aff60c19108ada7b
edriso <edriso.dev@gmail.com>
2023-11-04 16:19:16 +0200

    add another solution

--- Programming Skills/389. Find the Difference.js
+++ Programming Skills/389. Find the Difference.js
@@ -21,4 +21,16 @@ const findTheDifference = function (s, t) {
   return Object.keys(differenceObj).join('');
 };
 
+// Faster Solution by ChatGPT using XOR bitwise operation
+// const findTheDifference = function (s, t) {
+//   let result = 0;
+//   for (let char of s) {
+//     result ^= char.charCodeAt(0);
+//   }
+//   for (let char of t) {
+//     result ^= char.charCodeAt(0);
+//   }
+//   return String.fromCharCode(result);
+// };
+
 console.log(findTheDifference('hello world', 'hello zworld')); //z
