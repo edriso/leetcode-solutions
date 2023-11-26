9fe4548ba0761bbb5c0db6df2e22267c2b9de183
edriso <edriso.dev@gmail.com>
2023-11-26 22:27:22 +0200

    prepare a problem

---
+++ Programming Skills/1041. Robot Bounded In Circle.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/robot-bounded-in-circle/?envType=study-plan-v2&envId=programming-skills
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+const isRobotBounded = function (instructions) {
+  return false;
+};
+
+console.log(isRobotBounded("GGLLGG")); //true
+// console.log(isRobotBounded("GG")); //false
+// console.log(isRobotBounded("GL")); //true
