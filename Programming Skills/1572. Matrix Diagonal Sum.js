e53b9f601217860d5ac5cba4d78ca68332f5068d
edriso <edriso.dev@gmail.com>
2023-11-28 15:46:17 +0200

    prepare problems

---
+++ Programming Skills/1572. Matrix Diagonal Sum.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/matrix-diagonal-sum/?envType=study-plan-v2&envId=programming-skills
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+const diagonalSum = function (mat) {
+  let sum = 0;
+
+  //logic
+
+  return sum;
+};
+
+// console.log(diagonalSum([[1,2,3],
+//     [4,5,6],
+//     [7,8,9]])); //25
+// console.log(diagonalSum([[1,1,1,1],
+//     [1,1,1,1],
+//     [1,1,1,1],
+//     [1,1,1,1]])); //8
+// console.log(diagonalSum([[5]])); //5
