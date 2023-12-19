158b5a326021e936b59bfac520f3fa64248b6dc2
Mohamed Idris <edriso.dev@gmail.com>
2023-12-19 22:19:05 +0200

    solve a problem

--- Programming Skills/1572. Matrix Diagonal Sum.js
+++ Programming Skills/1572. Matrix Diagonal Sum.js
@@ -6,11 +6,27 @@
 const diagonalSum = function (mat) {
   let sum = 0;
 
-  //logic
+  for (let i = 0; i < mat.length; i++) {
+    sum += mat[i][i];
+    sum += mat[i][mat.length - i - 1];
+  }
+
+  if (mat.length % 2 !== 0) {
+    const mid = (1 + mat.length) / 2 - 1;
+    sum -= mat[mid][mid];
+  }
 
   return sum;
 };
 
+// console.log(
+//   diagonalSum([
+//     [7, 3, 1, 9],
+//     [3, 4, 6, 9],
+//     [6, 9, 6, 6],
+//     [9, 5, 8, 5],
+//   ])
+// ); //55
 // console.log(diagonalSum([[1,2,3],
 //     [4,5,6],
 //     [7,8,9]])); //25
